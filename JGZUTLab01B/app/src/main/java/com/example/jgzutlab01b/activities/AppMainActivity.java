9b1f360d530c2b8d84b681b2f4fca3bcc43f6239
jacob273 <jakubgmur@op.pl>
2021-03-20 11:55:22 +0100

    added main activity, and content_main view. added pl and eng lang for title & maincontent.

---
+++ JGZUTLab01B/app/src/main/java/com/example/jgzutlab01b/activities/AppMainActivity.java
@@ -0,0 +1,21 @@
+package com.example.jgzutlab01b.activities;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.jgzutlab01b.R;
+
+public class AppMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+    }
+
+
+
+}
