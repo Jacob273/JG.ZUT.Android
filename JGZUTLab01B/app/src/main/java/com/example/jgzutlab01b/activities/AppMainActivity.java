7d74ff171d6e9ea135f1edc816925d276d0e1a38
jacob273 <jakubgmur@op.pl>
2021-03-20 12:31:06 +0100

    AppMainActivity added setting locale

--- JGZUTLab01B/app/src/main/java/com/example/jgzutlab01b/activities/AppMainActivity.java
+++ JGZUTLab01B/app/src/main/java/com/example/jgzutlab01b/activities/AppMainActivity.java
@@ -1,5 +1,8 @@
 package com.example.jgzutlab01b.activities;
 
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
@@ -8,14 +11,24 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.jgzutlab01b.R;
 
+import java.util.Locale;
+
 public class AppMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setLocale(this, "en");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
     }
 
-
+    public static void setLocale(Activity activity, String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+        Resources resources = activity.getResources();
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+    }
 
 }
