c2aaf998ab28c07183c5e9ad1c4b6d94a81c7d19
Jakub Gmur <jakubgmur@op.pl>
2021-05-10 23:07:47 +0200

    initial commit for lab4

---
+++ Lab04/app/src/main/java/pl/jg/movingball/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package pl.jg.movingball.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.jg.movingball.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
